62663b085f43dbf009dc93f199aab2211d844cd0
benedito <benethowen@yahoo.com.br>
2021-10-09 23:01:59 -0300

    Correções das configuração da captação dos dados do formulário na route

--- blog/__init__.py
+++ blog/__init__.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt #para criptografar a senha
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meublog.db' #configurando o nome do Bd
-app.config['SECRET KEY'] = 'dscergthyyyjew987'
+app.config['SECRET_KEY'] = 'dscergthyyyjew987'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 from blog.admin import route
--- blog/admin/route.py
+++ blog/admin/route.py
@@ -1,6 +1,6 @@
 from flask import render_template, session, request, url_for, flash, redirect
 
-from form import RegistrationForm
+from .form import RegistrationForm
 
 from blog import app, db, bcrypt
 from .models import User
@@ -22,11 +22,11 @@ def registrar():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
         hash_password = bcrypt.generate_password_hash(form.password.data)
-        user = User(nome=form.name.data, username=form.username.data, email=form.email.data,
-        password=hash_password)
+        user = User(nome=form.name.data, email=form.email.data,
+        password = hash_password)
         
         db.session.add(user)
         flash('Obrigado por registrar')
-        return redirect(url_for('login'))
-    return render_template('admin/registrar.html', form=form)
+        return redirect(url_for('home'))
+    return render_template('admin/registrar.html', form=form, title= "Pagina de registro")
 
